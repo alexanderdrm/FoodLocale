63d61f209a4d1c74d189256f66596df732d0142b
Derek Alexander <dalexander@mobiquityinc.com>
2014-07-18 16:26:26 -0400

    detail screen filled out

--- src/main/java/com/mobiquity/localdel/CityInfoActivity.java
+++ src/main/java/com/mobiquity/localdel/CityInfoActivity.java
@@ -3,6 +3,8 @@ package com.mobiquity.localdel;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.example.LocalDel.R;
 
 /**
@@ -18,6 +20,16 @@ public class CityInfoActivity extends Activity {
         Intent intent = getIntent();
         int cityId = intent.getIntExtra("CityId",0);
         CityInfo cityInfo = CityFactory.getCityById(cityId);
+
+        TextView description = (TextView)findViewById(R.id.cityViewDescription);
+        TextView cityName = (TextView)findViewById(R.id.cityViewName);
+        ImageView image = (ImageView)findViewById(R.id.cityViewImage);
+
+        description.setText(cityInfo.getDescription());
+        cityName.setText(cityInfo.getSubtitleString());
+        image.setImageResource(R.drawable.taj_mahal);
+
+
     }
 
 }
